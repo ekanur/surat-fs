9b8b9a3f0bb6f8b9f1b00fdc880b6f3f6ffc74fd
Eka Nur Ahmad R <ekanur@um.ac.id>
2018-08-03 15:20:36 +0700

    edit menu admin

--- apps/resources/views/user/menus/admin.blade.php
+++ apps/resources/views/user/menus/admin.blade.php
@@ -6,20 +6,20 @@
 		</a>
 	</li>
 	<li class="">
-		<a href="{{ url("/layan-surat") }}">
-			<i class="now-ui-icons design_app"></i>
-			<p>Layanan Surat</p>
+		<a href="{{ url("/pejabat") }}">
+			<i class="now-ui-icons users_circle-08"></i>
+			<p>Pejabat</p>
 		</a>
 	</li>
 	<li class="">
 		<a href="{{ url("/mahasiswa") }}">
-			<i class="now-ui-icons design_app"></i>
+			<i class="now-ui-icons users_single-02"></i>
 			<p>Mahasiswa</p>
 		</a>
 	</li>
 	<li class="">
 		<a href="{{ url("/verifikator") }}">
-			<i class="now-ui-icons design_app"></i>
+			<i class="now-ui-icons files_single-copy-04"></i>
 			<p>Verifikator</p>
 		</a>
 	</li>
